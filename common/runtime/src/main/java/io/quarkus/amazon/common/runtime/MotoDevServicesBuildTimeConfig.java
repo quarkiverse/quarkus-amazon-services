00e8722fe3a4bcec5d2ca8ad3edec54eae5a572e
Sébastien CROCQUESEL <88554524+scrocquesel@users.noreply.github.com>
2022-09-26 14:58:10 +0000

    fix: remove unused properties

--- common/runtime/src/main/java/io/quarkus/amazon/common/runtime/MotoDevServicesBuildTimeConfig.java
+++ common/runtime/src/main/java/io/quarkus/amazon/common/runtime/MotoDevServicesBuildTimeConfig.java
@@ -9,19 +9,6 @@ import io.quarkus.runtime.annotations.ConfigRoot;
 @ConfigRoot(name = "aws.devservices.moto", phase = ConfigPhase.BUILD_AND_RUN_TIME_FIXED)
 public class MotoDevServicesBuildTimeConfig {
     /**
-     * If local stack based dev services should be used. This requires explicit
-     * config rather than
-     * being activated by a lack of config for dev services, as AWS is a singleton
-     * and does not seen
-     * to be configured in the same way as other services.
-     *
-     * If this is true then a localstack container will be started and will be
-     * used instead of AWS.
-     */
-    @ConfigItem
-    public boolean enabled;
-
-    /**
      * The localstack container image to use.
      */
     @ConfigItem(defaultValue = "motoserver/moto")
