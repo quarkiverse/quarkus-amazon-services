0c789e71eefa7879e0af36723b599315a54526e5
Sébastien Crocquesel <88554524+scrocquesel@users.noreply.github.com>
2023-07-03 20:57:36 +0200

    fix: remove non necessary target context

--- dynamodb-enhanced/runtime/src/main/java/io/quarkus/amazon/dynamodb/enhanced/runtime/NamedDynamoDbTable.java
+++ dynamodb-enhanced/runtime/src/main/java/io/quarkus/amazon/dynamodb/enhanced/runtime/NamedDynamoDbTable.java
@@ -1,8 +1,6 @@
 package io.quarkus.amazon.dynamodb.enhanced.runtime;
 
 import static java.lang.annotation.ElementType.FIELD;
-import static java.lang.annotation.ElementType.METHOD;
-import static java.lang.annotation.ElementType.PARAMETER;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 import java.lang.annotation.Documented;
@@ -14,7 +12,7 @@ import jakarta.inject.Qualifier;
 /**
  * Specification of DynamoDb table to be injected.
  */
-@Target({ FIELD, METHOD, PARAMETER })
+@Target({ FIELD })
 @Retention(RUNTIME)
 @Qualifier
 @Documented
