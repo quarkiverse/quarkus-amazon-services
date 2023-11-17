91d68728f66ff2d0e7c74189f4e0fe21a381420d
Sébastien Crocquesel <88554524+scrocquesel@users.noreply.github.com>
2023-11-17 19:33:21 +0100

    build(deps): bump localstack from 2.3.0 to 3.0.1

--- common/runtime/src/main/java/io/quarkus/amazon/common/runtime/LocalStackDevServicesBuildTimeConfig.java
+++ common/runtime/src/main/java/io/quarkus/amazon/common/runtime/LocalStackDevServicesBuildTimeConfig.java
@@ -14,7 +14,7 @@ public interface LocalStackDevServicesBuildTimeConfig {
     /**
      * The LocalStack container image to use.
      */
-    @WithDefault(value = "localstack/localstack:2.3.0")
+    @WithDefault(value = "localstack/localstack:3.0.1")
     String imageName();
 
     /**
