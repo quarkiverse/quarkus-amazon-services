556f33655bb2c5c16fc38a30702fd43a65b76852
triphon <triphon@gmail.com>
2023-11-18 09:25:04 +0200

    Quarkus lambda client - initial version

---
+++ lambdaclient/runtime/src/main/java/io/quarkus/amazon/lambda/client/runtime/LambdaBuildTimeConfig.java
@@ -0,0 +1,20 @@
+package io.quarkus.amazon.lambda.client.runtime;
+
+import io.quarkus.amazon.common.runtime.AsyncHttpClientBuildTimeConfig;
+import io.quarkus.amazon.common.runtime.HasSdkBuildTimeConfig;
+import io.quarkus.amazon.common.runtime.SyncHttpClientBuildTimeConfig;
+import io.quarkus.runtime.annotations.ConfigPhase;
+import io.quarkus.runtime.annotations.ConfigRoot;
+import io.smallrye.config.ConfigMapping;
+
+/** Amazon LAMBDA build time configuration */
+@ConfigMapping(prefix = "quarkus.lambda")
+@ConfigRoot(phase = ConfigPhase.BUILD_AND_RUN_TIME_FIXED)
+public interface LambdaBuildTimeConfig extends HasSdkBuildTimeConfig {
+
+    /** Sync HTTP transport configuration for Amazon LAMBDA client */
+    SyncHttpClientBuildTimeConfig syncClient();
+
+    /** Async HTTP transport configuration for Amazon LAMBDA client */
+    AsyncHttpClientBuildTimeConfig asyncClient();
+}
