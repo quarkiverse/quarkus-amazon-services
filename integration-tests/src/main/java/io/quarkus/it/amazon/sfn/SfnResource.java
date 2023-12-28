6bff3014188545e779254fd2d1354a27de1ed4ea
Bas Hovestad <bas.hovestad@abnamroverzekeringen.nl>
2023-12-28 22:01:42 +0100

    Added integration test

---
+++ integration-tests/src/main/java/io/quarkus/it/amazon/sfn/SfnResource.java
@@ -0,0 +1,75 @@
+package io.quarkus.it.amazon.sfn;
+
+import static jakarta.ws.rs.core.MediaType.TEXT_PLAIN;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.function.Consumer;
+
+import jakarta.inject.Inject;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+
+import org.jboss.logging.Logger;
+
+import software.amazon.awssdk.services.sfn.SfnAsyncClient;
+import software.amazon.awssdk.services.sfn.SfnClient;
+import software.amazon.awssdk.services.sfn.model.CreateStateMachineRequest;
+import software.amazon.awssdk.services.sfn.model.CreateStateMachineResponse;
+
+@Path("/sfn")
+public class SfnResource {
+
+    private static final Logger LOG = Logger.getLogger(SfnResource.class);
+
+    @Inject
+    SfnClient sfnClient;
+
+    @Inject
+    SfnAsyncClient sfnAsyncClient;
+
+    @GET
+    @Path("sync")
+    @Produces(TEXT_PLAIN)
+    public String testSync() {
+        LOG.info("Testing Sync SFN client");
+        CreateStateMachineResponse stateMachine = sfnClient.createStateMachine(
+                stateMachineRequest("sync-state-machine"));
+
+        return stateMachine.stateMachineArn();
+    }
+
+    @GET
+    @Path("async")
+    @Produces(TEXT_PLAIN)
+    public String testAsync() throws InterruptedException, ExecutionException {
+        LOG.info("Testing Async SFN client");
+
+        CompletableFuture<CreateStateMachineResponse> stateMachine = sfnAsyncClient
+                .createStateMachine(stateMachineRequest("async-state-machine"));
+
+        return stateMachine.get().stateMachineArn();
+    }
+
+    private Consumer<CreateStateMachineRequest.Builder> stateMachineRequest(String name) {
+        return builder -> builder
+                .name(name)
+                .definition(stateMachineDefinition())
+                .build();
+    }
+
+    private String stateMachineDefinition() {
+        return "{\n" +
+                "  \"Comment\": \"A Hello World example of the Amazon States Language using Pass states\",\n" +
+                "  \"StartAt\": \"HelloWorld\",\n" +
+                "  \"States\": {\n" +
+                "    \"HelloWorld\": {\n" +
+                "      \"Type\": \"Pass\",\n" +
+                "      \"Result\": \"Hello World!\",\n" +
+                "      \"End\": true\n" +
+                "    }\n" +
+                "  }\n" +
+                "}";
+    }
+}
--- integration-tests/src/test/java/io/quarkus/it/amazon/AmazonSfnTest.java
+++ integration-tests/src/test/java/io/quarkus/it/amazon/AmazonSfnTest.java
@@ -1,20 +1,25 @@
 package io.quarkus.it.amazon;
 
-import static org.hamcrest.Matchers.any;
-
-import org.junit.jupiter.api.Test;
-
 import io.quarkus.test.junit.QuarkusTest;
 import io.restassured.RestAssured;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.hamcrest.Matchers.is;
 
 @QuarkusTest
 public class AmazonSfnTest {
 
-    @Test
-    public void testSfnSync() {
-        RestAssured.when()
-                .get("/test/sfn/sync")
+    @ParameterizedTest
+    @ValueSource(strings = {"sync", "async"})
+    public void testSfn(String endpoint) {
+        final String expectedBody = String.format("arn:aws:states:us-east-1:000000000000:stateMachine:%s-state-machine", endpoint);
+
+        RestAssured.given()
+                .pathParam("endpoint", endpoint)
+                .when().get("/test/sfn/{endpoint}")
                 .then()
-                .body(any(String.class));
+                .body(is(expectedBody));
     }
+
 }
