b2a39dfdc77d183aaca69e9a063e96292f286fac
André Zimmermann <a.zimmermann@univapay.com>
2024-03-18 15:41:26 +0900

    Review tests

---
+++ integration-tests/src/test/java/io/quarkus/it/amazon/AmazonCloudWatchLogsITCase.java
@@ -0,0 +1,7 @@
+package io.quarkus.it.amazon;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class AmazonCloudWatchLogsITCase extends AmazonCloudWatchLogTest {
+}
