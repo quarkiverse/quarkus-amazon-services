1c19c2cf3a8c342965e61e77bf8ad629c2747709
Eugene Berman <eugene.berman@portx.io>
2024-08-22 07:03:05 -0700

    Support for the ECR client #1371

---
+++ ecr/runtime/src/main/java/io/quarkus/amazon/ecr/runtime/EcrRecorder.java
@@ -0,0 +1,53 @@
+package io.quarkus.amazon.ecr.runtime;
+
+import io.quarkus.amazon.common.runtime.AmazonClientRecorder;
+import io.quarkus.amazon.common.runtime.AsyncHttpClientConfig;
+import io.quarkus.amazon.common.runtime.AwsConfig;
+import io.quarkus.amazon.common.runtime.SdkConfig;
+import io.quarkus.amazon.common.runtime.SyncHttpClientConfig;
+import io.quarkus.runtime.RuntimeValue;
+import io.quarkus.runtime.annotations.Recorder;
+import software.amazon.awssdk.awscore.client.builder.AwsAsyncClientBuilder;
+import software.amazon.awssdk.awscore.client.builder.AwsSyncClientBuilder;
+import software.amazon.awssdk.services.ecr.EcrAsyncClient;
+import software.amazon.awssdk.services.ecr.EcrClient;
+
+@Recorder
+public class EcrRecorder extends AmazonClientRecorder {
+
+    final EcrConfig config;
+
+    public EcrRecorder(EcrConfig config) {
+        this.config = config;
+    }
+
+    @Override
+    public RuntimeValue<AwsConfig> getAwsConfig() {
+        return new RuntimeValue<>(config.aws());
+    }
+
+    @Override
+    public RuntimeValue<SdkConfig> getSdkConfig() {
+        return new RuntimeValue<>(config.sdk());
+    }
+
+    @Override
+    public AsyncHttpClientConfig getAsyncClientConfig() {
+        return config.asyncClient();
+    }
+
+    @Override
+    public SyncHttpClientConfig getSyncClientConfig() {
+        return config.syncClient();
+    }
+
+    @Override
+    public AwsSyncClientBuilder<?, ?> geSyncClientBuilder() {
+        return EcrClient.builder();
+    }
+
+    @Override
+    public AwsAsyncClientBuilder<?, ?> getAsyncClientBuilder() {
+        return EcrAsyncClient.builder();
+    }
+}
