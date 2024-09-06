1b2c566a6ae1d07f0751e573039c260110b5ba7b
Sébastien Crocquesel <88554524+scrocquesel@users.noreply.github.com>
2024-09-06 20:14:45 +0200

    Add a devservices test to ensure pro version is not required

--- devservices/ecr/src/main/java/io/quarkus/amazon/devservices/ecr/EcrDevServicesProcessor.java
+++ devservices/ecr/src/main/java/io/quarkus/amazon/devservices/ecr/EcrDevServicesProcessor.java
@@ -1,15 +1,16 @@
 package io.quarkus.amazon.devservices.ecr;
 
+import org.testcontainers.containers.localstack.LocalStackContainer.EnabledService;
+
 import io.quarkus.amazon.common.deployment.spi.AbstractDevServicesLocalStackProcessor;
 import io.quarkus.amazon.common.deployment.spi.DevServicesLocalStackProviderBuildItem;
 import io.quarkus.amazon.ecr.runtime.EcrBuildTimeConfig;
 import io.quarkus.deployment.annotations.BuildStep;
-import org.testcontainers.containers.localstack.LocalStackContainer;
 
 public class EcrDevServicesProcessor extends AbstractDevServicesLocalStackProcessor {
 
     @BuildStep
     DevServicesLocalStackProviderBuildItem setupEcr(EcrBuildTimeConfig clientBuildTimeConfig) {
-        return this.setup(LocalStackContainer.EnabledService.named("ecr"), clientBuildTimeConfig.devservices());
+        return this.setup(EnabledService.named("ecr"), clientBuildTimeConfig.devservices());
     }
 }
