089190007562ebfaf700b3c236017d15958d9629
Sébastien Crocquesel <88554524+scrocquesel@users.noreply.github.com>
2024-10-23 21:11:19 +0200

    feat: add payment cryptography extension

---
+++ paymentcryptographydata/runtime/src/main/java/io/quarkiverse/amazon/paymentcryptographydata/runtime/PaymentCryptographyDataConfig.java
@@ -0,0 +1,25 @@
+package io.quarkiverse.amazon.paymentcryptographydata.runtime;
+
+import io.quarkiverse.amazon.common.runtime.AsyncHttpClientConfig;
+import io.quarkiverse.amazon.common.runtime.HasAmazonClientRuntimeConfig;
+import io.quarkiverse.amazon.common.runtime.SyncHttpClientConfig;
+import io.quarkus.runtime.annotations.ConfigDocSection;
+import io.quarkus.runtime.annotations.ConfigPhase;
+import io.quarkus.runtime.annotations.ConfigRoot;
+import io.smallrye.config.ConfigMapping;
+
+@ConfigMapping(prefix = "quarkus.paymentcryptographydata")
+@ConfigRoot(phase = ConfigPhase.RUN_TIME)
+public interface PaymentCryptographyDataConfig extends HasAmazonClientRuntimeConfig {
+    /**
+     * Sync HTTP transport configurations
+     */
+    @ConfigDocSection
+    SyncHttpClientConfig syncClient();
+
+    /**
+     * Async HTTP transport configurations
+     */
+    @ConfigDocSection
+    AsyncHttpClientConfig asyncClient();
+}
