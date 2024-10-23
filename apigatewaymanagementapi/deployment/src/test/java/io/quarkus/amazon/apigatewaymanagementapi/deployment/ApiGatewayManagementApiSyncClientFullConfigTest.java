b0604dfed992ee5b7f37484f8a396ac5411215c2
Sébastien Crocquesel <88554524+scrocquesel@users.noreply.github.com>
2024-10-23 21:55:17 +0200

    feat: add api gateway management api extension (#1451)

---
+++ apigatewaymanagementapi/deployment/src/test/java/io/quarkus/amazon/apigatewaymanagementapi/deployment/ApiGatewayManagementApiSyncClientFullConfigTest.java
@@ -0,0 +1,29 @@
+package io.quarkus.amazon.apigatewaymanagementapi.deployment;
+
+import jakarta.inject.Inject;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
+
+import io.quarkus.test.QuarkusUnitTest;
+import software.amazon.awssdk.services.apigatewaymanagementapi.ApiGatewayManagementApiAsyncClient;
+import software.amazon.awssdk.services.apigatewaymanagementapi.ApiGatewayManagementApiClient;
+
+public class ApiGatewayManagementApiSyncClientFullConfigTest {
+
+    @Inject
+    ApiGatewayManagementApiClient client;
+
+    @Inject
+    ApiGatewayManagementApiAsyncClient async;
+
+    @RegisterExtension
+    static final QuarkusUnitTest config = new QuarkusUnitTest()
+            .withApplicationRoot((jar) -> jar
+                    .addAsResource("sync-urlconn-full-config.properties", "application.properties"));
+
+    @Test
+    public void test() {
+        // should finish with success
+    }
+}
