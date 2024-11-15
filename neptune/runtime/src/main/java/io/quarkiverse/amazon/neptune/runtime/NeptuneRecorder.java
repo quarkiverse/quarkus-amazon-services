b090b646e3a4d8928aa192490124ba59846d1185
Sébastien Crocquesel <88554524+scrocquesel@users.noreply.github.com>
2024-11-15 23:57:33 +0100

    feat: add neptune extension

---
+++ neptune/runtime/src/main/java/io/quarkiverse/amazon/neptune/runtime/NeptuneRecorder.java
@@ -0,0 +1,47 @@
+package io.quarkiverse.amazon.neptune.runtime;
+
+import io.quarkiverse.amazon.common.runtime.AmazonClientRecorder;
+import io.quarkiverse.amazon.common.runtime.AsyncHttpClientConfig;
+import io.quarkiverse.amazon.common.runtime.HasAmazonClientRuntimeConfig;
+import io.quarkiverse.amazon.common.runtime.SyncHttpClientConfig;
+import io.quarkus.runtime.RuntimeValue;
+import io.quarkus.runtime.annotations.Recorder;
+import software.amazon.awssdk.awscore.client.builder.AwsAsyncClientBuilder;
+import software.amazon.awssdk.awscore.client.builder.AwsSyncClientBuilder;
+import software.amazon.awssdk.services.neptune.NeptuneAsyncClient;
+import software.amazon.awssdk.services.neptune.NeptuneClient;
+
+@Recorder
+public class NeptuneRecorder extends AmazonClientRecorder {
+
+    final NeptuneConfig config;
+
+    public NeptuneRecorder(NeptuneConfig config) {
+        this.config = config;
+    }
+
+    @Override
+    public RuntimeValue<HasAmazonClientRuntimeConfig> getAmazonClientsConfig() {
+        return new RuntimeValue<>(config);
+    }
+
+    @Override
+    public AsyncHttpClientConfig getAsyncClientConfig() {
+        return config.asyncClient();
+    }
+
+    @Override
+    public SyncHttpClientConfig getSyncClientConfig() {
+        return config.syncClient();
+    }
+
+    @Override
+    public AwsSyncClientBuilder<?, ?> geSyncClientBuilder() {
+        return NeptuneClient.builder();
+    }
+
+    @Override
+    public AwsAsyncClientBuilder<?, ?> getAsyncClientBuilder() {
+        return NeptuneAsyncClient.builder();
+    }
+}
