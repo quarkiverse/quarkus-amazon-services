a21c8bca93557062c66eda073bce7f881f6fa206
adampoplawski <adampoplaw@gmail.com>
2024-11-16 21:47:52 +0100

    Added integration tests for SQS Reactive Messaging (#1483)
    Co-authored-by: adam.poplawski@tui.co.uk <adam.poplawski@tui.co.uk>

---
+++ integration-tests/src/main/java/io/quarkus/it/amazon/sqsmessagingconnector/SqsMessagingConnectorResource.java
@@ -0,0 +1,28 @@
+package io.quarkus.it.amazon.sqsmessagingconnector;
+
+import java.util.List;
+
+import jakarta.inject.Inject;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+
+import org.jboss.resteasy.reactive.RestQuery;
+
+@Path("/sqs-messaging-connector")
+public class SqsMessagingConnectorResource {
+    @Inject
+    SqsMessagingConnectorManager connectorManager;
+
+    @Path("messages/{queueName}")
+    @POST
+    public void sendSyncMessage(String queueName, @RestQuery String message) {
+        connectorManager.sendMessage(message, queueName);
+    }
+
+    @Path("messages/{queueName}")
+    @GET
+    public List<String> getMessages() {
+        return connectorManager.getMessages();
+    }
+}
---
+++ integration-tests/src/test/java/io/quarkus/it/amazon/AmazonSqsMessagingConnectorTest.java
@@ -0,0 +1,55 @@
+package io.quarkus.it.amazon;
+
+import static io.restassured.RestAssured.given;
+import static org.awaitility.Awaitility.await;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.any;
+import static org.hamcrest.Matchers.arrayWithSize;
+import static org.hamcrest.Matchers.is;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.ws.rs.core.Response;
+
+import org.junit.jupiter.api.Test;
+
+import io.quarkus.test.junit.QuarkusTest;
+
+@QuarkusTest
+public class AmazonSqsMessagingConnectorTest {
+
+    private final static String QUEUE_NAME = "quarkus-messaging-test-queue";
+    private final static List<String> MESSAGES = new ArrayList<>();
+
+    static {
+        MESSAGES.add("First Message");
+        MESSAGES.add("Second Message");
+        MESSAGES.add("Third Message");
+    }
+
+    @Test
+    public void testReceiveMessages() {
+        //Publish messages
+        MESSAGES.forEach(msg -> {
+            given()
+                    .pathParam("queueName", QUEUE_NAME)
+                    .queryParam("message", msg)
+                    .when().post("/test/sqs-messaging-connector/messages/{queueName}")
+                    .then().body(any(String.class));
+        });
+
+        await()
+                .atMost(Duration.ofSeconds(10L))
+                .pollInterval(Duration.ofSeconds(1L))
+                .untilAsserted(() -> assertThat(given()
+                        .pathParam("queueName", QUEUE_NAME)
+                        .get("/test/sqs-messaging-connector/messages/{queueName}")
+                        .then()
+                        .assertThat()
+                        .statusCode(is(Response.Status.OK.getStatusCode()))
+                        .extract()
+                        .as(String[].class), arrayWithSize(3)));
+    }
+}
