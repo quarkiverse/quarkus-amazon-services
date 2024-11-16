4bcf46e847e9d2eb1cfdd293d46f7141fd21d857
adampoplawski <adampoplaw@gmail.com>
2024-11-16 21:47:52 +0100

    Added integration tests for SQS Reactive Messaging (#1483)
    Co-authored-by: adam.poplawski@tui.co.uk <adam.poplawski@tui.co.uk>

---
+++ integration-tests/src/main/java/io/quarkiverse/it/amazon/sqsmessagingconnector/SqsMessagingConnectorManager.java
@@ -0,0 +1,36 @@
+package io.quarkiverse.it.amazon.sqsmessagingconnector;
+
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+
+import org.eclipse.microprofile.reactive.messaging.Incoming;
+
+import software.amazon.awssdk.services.sqs.SqsClient;
+import software.amazon.awssdk.services.sqs.model.SendMessageRequest;
+
+@ApplicationScoped
+public class SqsMessagingConnectorManager {
+    @Inject
+    SqsClient sqsClient;
+    List<String> messages = new CopyOnWriteArrayList<>();
+
+    @Incoming("messages")
+    public void process(String incomingMessage) {
+        messages.add(incomingMessage);
+    }
+
+    public void sendMessage(String message, String queueName) {
+        sqsClient.sendMessage(SendMessageRequest.builder().queueUrl(getQueueUrl(queueName)).messageBody(message).build());
+    }
+
+    public List<String> getMessages() {
+        return messages;
+    }
+
+    private String getQueueUrl(String queueName) {
+        return sqsClient.listQueues(r -> r.queueNamePrefix(queueName)).queueUrls().get(0);
+    }
+}
