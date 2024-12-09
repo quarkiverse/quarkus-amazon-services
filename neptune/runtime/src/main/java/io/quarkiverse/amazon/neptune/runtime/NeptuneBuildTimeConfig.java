bc986abcae3ec64c882f7936d30302718ff3656d
Sébastien Crocquesel <88554524+scrocquesel@users.noreply.github.com>
2024-12-09 20:42:25 +0100

    feat: remove coupling with optional client implementation

--- neptune/runtime/src/main/java/io/quarkiverse/amazon/neptune/runtime/NeptuneBuildTimeConfig.java
+++ neptune/runtime/src/main/java/io/quarkiverse/amazon/neptune/runtime/NeptuneBuildTimeConfig.java
@@ -1,9 +1,8 @@
 package io.quarkiverse.amazon.neptune.runtime;
 
-import io.quarkiverse.amazon.common.runtime.AsyncHttpClientBuildTimeConfig;
 import io.quarkiverse.amazon.common.runtime.DevServicesBuildTimeConfig;
 import io.quarkiverse.amazon.common.runtime.HasSdkBuildTimeConfig;
-import io.quarkiverse.amazon.common.runtime.SyncHttpClientBuildTimeConfig;
+import io.quarkiverse.amazon.common.runtime.HasTransportBuildTimeConfig;
 import io.quarkus.runtime.annotations.ConfigPhase;
 import io.quarkus.runtime.annotations.ConfigRoot;
 import io.smallrye.config.ConfigMapping;
@@ -13,17 +12,7 @@ import io.smallrye.config.ConfigMapping;
  */
 @ConfigMapping(prefix = "quarkus.neptune")
 @ConfigRoot(phase = ConfigPhase.BUILD_AND_RUN_TIME_FIXED)
-public interface NeptuneBuildTimeConfig extends HasSdkBuildTimeConfig {
-
-    /**
-     * Sync HTTP transport configuration for Amazon Neptune client
-     */
-    SyncHttpClientBuildTimeConfig syncClient();
-
-    /**
-     * Async HTTP transport configuration for Amazon Neptune client
-     */
-    AsyncHttpClientBuildTimeConfig asyncClient();
+public interface NeptuneBuildTimeConfig extends HasSdkBuildTimeConfig, HasTransportBuildTimeConfig {
 
     /**
      * Config for dev services
