67766a0fc40831c6e104246e39e2889c518b7f0e
Sébastien Crocquesel <88554524+scrocquesel@users.noreply.github.com>
2024-12-09 20:42:25 +0100

    feat: remove coupling with optional client implementation

--- apigatewaymanagementapi/runtime/src/main/java/io/quarkiverse/amazon/apigatewaymanagementapi/runtime/ApiGatewayManagementApiBuildTimeConfig.java
+++ apigatewaymanagementapi/runtime/src/main/java/io/quarkiverse/amazon/apigatewaymanagementapi/runtime/ApiGatewayManagementApiBuildTimeConfig.java
@@ -1,9 +1,8 @@
 package io.quarkiverse.amazon.apigatewaymanagementapi.runtime;
 
-import io.quarkiverse.amazon.common.runtime.AsyncHttpClientBuildTimeConfig;
 import io.quarkiverse.amazon.common.runtime.DevServicesBuildTimeConfig;
 import io.quarkiverse.amazon.common.runtime.HasSdkBuildTimeConfig;
-import io.quarkiverse.amazon.common.runtime.SyncHttpClientBuildTimeConfig;
+import io.quarkiverse.amazon.common.runtime.HasTransportBuildTimeConfig;
 import io.quarkus.runtime.annotations.ConfigPhase;
 import io.quarkus.runtime.annotations.ConfigRoot;
 import io.smallrye.config.ConfigMapping;
@@ -13,17 +12,7 @@ import io.smallrye.config.ConfigMapping;
  */
 @ConfigMapping(prefix = "quarkus.apigatewaymanagementapi")
 @ConfigRoot(phase = ConfigPhase.BUILD_AND_RUN_TIME_FIXED)
-public interface ApiGatewayManagementApiBuildTimeConfig extends HasSdkBuildTimeConfig {
-
-    /**
-     * Sync HTTP transport configuration for Amazon Api Gateway Management Api client
-     */
-    SyncHttpClientBuildTimeConfig syncClient();
-
-    /**
-     * Async HTTP transport configuration for Amazon Api Gateway Management Api client
-     */
-    AsyncHttpClientBuildTimeConfig asyncClient();
+public interface ApiGatewayManagementApiBuildTimeConfig extends HasSdkBuildTimeConfig, HasTransportBuildTimeConfig {
 
     /**
      * Config for dev services
