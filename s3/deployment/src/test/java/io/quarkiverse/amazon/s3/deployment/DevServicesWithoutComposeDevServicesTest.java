93570f195b6702c683b577f32714e400cd7286f0
Maximilian Zellhofer <max.zellhofer@gmail.com>
2025-09-29 20:35:05 +0200

    feat: use compose dev services network when available (#1862)
    * fix: use compose dev services network if possible
    
    * chore: add tests
    
    * chore: implement review

---
+++ s3/deployment/src/test/java/io/quarkiverse/amazon/s3/deployment/DevServicesWithoutComposeDevServicesTest.java
@@ -0,0 +1,26 @@
+package io.quarkiverse.amazon.s3.deployment;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
+import org.testcontainers.DockerClientFactory;
+
+import io.quarkus.test.QuarkusUnitTest;
+
+public class DevServicesWithoutComposeDevServicesTest {
+
+    @RegisterExtension
+    static final QuarkusUnitTest config = new QuarkusUnitTest()
+            .withEmptyApplication();
+
+    @Test
+    public void test() {
+        var networkName = DockerClientFactory.lazyClient().listContainersCmd().exec().stream()
+                .filter(container -> container.getImage().contains("localstack")).toList()
+                .get(0)
+                .getNetworkSettings().getNetworks().keySet().stream().toList().get(0);
+        assertFalse(
+                networkName.startsWith("quarkus-devservices-quarkus-amazon-s3-deployment") && networkName.endsWith("_default"));
+    }
+}
