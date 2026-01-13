6ad4c00c8f4d83a6fc77b894c9ecf2f69f655633
Sébastien Crocquesel <88554524+scrocquesel@users.noreply.github.com>
2026-01-13 20:04:16 +0000

    feat: add transfer extension

---
+++ services/transfer/deployment/src/test/java/io/quarkiverse/amazon/transfer/deployment/TransferClientFullConfigTest.java
@@ -0,0 +1,31 @@
+package io.quarkiverse.amazon.transfer.deployment;
+
+import jakarta.inject.Inject;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
+
+import io.quarkus.test.QuarkusUnitTest;
+import software.amazon.awssdk.services.transfer.TransferAsyncClient;
+import software.amazon.awssdk.services.transfer.TransferClient;
+
+public class TransferClientFullConfigTest {
+
+    @Inject
+    TransferClient client;
+
+    @Inject
+    TransferAsyncClient async;
+
+    @RegisterExtension
+    static final QuarkusUnitTest config = new QuarkusUnitTest()
+            .withApplicationRoot((jar) -> jar
+                    .addAsResource("full-config.properties", "application.properties"));
+
+    @Test
+    public void test() {
+        Assertions.assertNotNull(client);
+        Assertions.assertNotNull(async);
+    }
+}
