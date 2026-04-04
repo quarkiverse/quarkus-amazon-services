422c683925a43557ae17f59969e6b7c967b5e5be
Sébastien Crocquesel <88554524+scrocquesel@users.noreply.github.com>
2026-04-04 19:10:17 +0000

    test: fix assertions for extensions using http/2 with aws crt

--- integration-tests/src/test/java/io/quarkiverse/it/amazon/AmazonBedrockRuntimeTest.java
+++ integration-tests/src/test/java/io/quarkiverse/it/amazon/AmazonBedrockRuntimeTest.java
@@ -1,12 +1,11 @@
 package io.quarkiverse.it.amazon;
 
-import static org.hamcrest.Matchers.anyOf;
 import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.oneOf;
 
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+import org.junit.jupiter.api.Test;
 
 import io.quarkus.test.junit.QuarkusTest;
 import io.restassured.RestAssured;
@@ -15,16 +14,36 @@ import io.restassured.parsing.Parser;
 @QuarkusTest
 class AmazonBedrockRuntimeTest {
 
-    @ParameterizedTest
-    @ValueSource(strings = { "sync", "async" })
-    void testPublishAndReceive(String endpoint) {
+    @ConfigProperty(name = "sync-client-type", defaultValue = "")
+    String syncClientType;
+
+    @Test
+    void testPublishAndReceiveAsync() {
         RestAssured.defaultParser = Parser.JSON;
 
         // List invoked
-        RestAssured.given().pathParam("endpoint", endpoint).when()
+        RestAssured.given().pathParam("endpoint", "async").when()
                 .get("/test/bedrockruntime/{endpoint}/list-invokes")
                 // test at least for pro feature failure
-                .then().statusCode(oneOf(501, 500)).body(anyOf(is(""), containsString(
-                        "HTTP/2 is not supported in AwsCrtHttpClient yet. Use NettyNioAsyncHttpClient instead")));
+                .then().statusCode(oneOf(501)).body(is(""));
+    }
+
+    @Test
+    void testPublishAndReceiveSync() {
+        RestAssured.defaultParser = Parser.JSON;
+
+        if ("aws-crt".equals(syncClientType)) {
+            // AWS CRT doesn't support HTTP/2
+            RestAssured.given().pathParam("endpoint", "sync").when()
+                    .get("/test/bedrockruntime/{endpoint}/list-invokes")
+                    .then().statusCode(500)
+                    .body(containsString(
+                            "HTTP/2 is not supported for sync HTTP clients. Either use HTTP/1.1 (the default) or use an async HTTP client (e.g., AwsCrtAsyncHttpClient)."));
+        } else {
+            // Other sync clients should work fine
+            RestAssured.given().pathParam("endpoint", "sync").when()
+                    .get("/test/bedrockruntime/{endpoint}/list-invokes")
+                    .then().statusCode(501).body(is(""));
+        }
     }
 }
