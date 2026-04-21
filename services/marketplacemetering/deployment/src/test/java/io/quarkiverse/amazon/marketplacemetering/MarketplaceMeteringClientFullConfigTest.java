3a230ee74e576e8cbf0658c37ed69eb05a982f0e
Tim Hurman <tim.hurman@hamoni.ai>
2026-04-21 10:50:01 +0900

    Added Markplace Metering client

---
+++ services/marketplacemetering/deployment/src/test/java/io/quarkiverse/amazon/marketplacemetering/MarketplaceMeteringClientFullConfigTest.java
@@ -0,0 +1,31 @@
+package io.quarkiverse.amazon.marketplacemetering;
+
+import jakarta.inject.Inject;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
+
+import io.quarkus.test.QuarkusUnitTest;
+import software.amazon.awssdk.services.marketplacemetering.MarketplaceMeteringAsyncClient;
+import software.amazon.awssdk.services.marketplacemetering.MarketplaceMeteringClient;
+
+class MarketplaceMeteringClientFullConfigTest {
+
+    @Inject
+    MarketplaceMeteringClient client;
+
+    @Inject
+    MarketplaceMeteringAsyncClient async;
+
+    @RegisterExtension
+    static final QuarkusUnitTest config = new QuarkusUnitTest()
+            .withApplicationRoot((jar) -> jar
+                    .addAsResource("full-config.properties", "application.properties"));
+
+    @Test
+    void test() {
+        Assertions.assertNotNull(client);
+        Assertions.assertNotNull(async);
+    }
+}
